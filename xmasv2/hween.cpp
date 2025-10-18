6f84ddfe9a054d214255b16878dc43a666e6f318
Cass Everitt <cass.everitt@gmail.com>
2025-10-18 08:18:30 -0600

    foo

--- xmasv2/hween.cpp
+++ xmasv2/hween.cpp
@@ -6,9 +6,9 @@
 #include <unistd.h>
 #include <vector>
 
-#include <linear.h>
-
 #include "smi_leds.h"
+
+#include "linear.h"
 
 using namespace r3;
 
@@ -55,51 +55,15 @@ void set_color(uint8_t *&ptr, const Vec3f &color) {
   *(ptr++) = (int)(color.z * 255);
 }
 
-int main(int argc, char *argv[]) {
-  uint8_t buffer[NUM_LEDS * NUM_STRIPS * 3] = {};
-  uint8_t *ptr;
-
-  std::vector<int> strip_indexes = {0, 3, 6, 9, 14};
-
-  // initialize the smi_leds module, starting with a 35% brightness
-  leds_init(NUM_LEDS, 35);
-  printf("compiled for %d strips\n", leds_num_strips());
-
-  int64_t start = get_time();
-  int count = 0;
-  int64_t freecount = 0;
-  for (float t = 0.0;; t += .0001) {
-    // Manually and slowly build the color buffer. 3 bytes per pixel (RGB)
-    // for NUM_LEDS pixels and NUM_STRIPS strips
-    ptr = buffer;
-    for (int strip = 0; strip < NUM_STRIPS; strip++) {
-      float hue = fmod(t + (strip * .04), 1.0);
-      float r, g, b;
-
-      Vec3f rgb = rgb_from_hsv(Vec3f(hue, 1.0, 1.0));
-      hsv_to_rgb(hue, 1.0, 1.0, &r, &g, &b);
-      for (int led = 0; led < NUM_LEDS; led++) {
-        set_color(ptr, rgb);
-      }
-
-      uint8_t *pixels = buffer + (strip * NUM_LEDS * 3);
-      Vec3f red(50 / 255.0f, 0, 0);
-      for (int led = 0; led < strip; led++) {
-        set_color(pixels, red);
-      }
-    }
-}
-
-
 int main(int argc, char *argv[])
 {
     uint8_t buffer[NUM_LEDS * NUM_STRIPS * 3] = {};
     uint8_t *ptr;
 
-    std::vector<int> strip_indexes = {0, 3, 6, 9, 14};
+    std::vector<int> strip_indexes = {0, 1, 2, 3, 4, 5};
 
     // initialize the smi_leds module, starting with a 35% brightness
-    leds_init(NUM_LEDS, 35);
+    leds_init(NUM_LEDS, 296);
     printf("compiled for %d strips\n", leds_num_strips());
 
     int64_t start = get_time();
@@ -112,26 +76,21 @@ int main(int argc, char *argv[])
         ptr = buffer;
         for(int strip = 0; strip < NUM_STRIPS; strip++)
         {
-            float hue = fmod(t + (strip * .04), 1.0);
-            float r, g, b;
+      float hue = fmod(t + (strip * .04), 1.0);
 
-            hsv_to_rgb(hue, 1.0, 1.0, &r, &g, &b);
-            for(int led = 0; led < NUM_LEDS; led++)
-            {
-                *(ptr++) = (int)(r * 255);
-                *(ptr++) = (int)(g * 255);
-                *(ptr++) = (int)(b * 255);
-            }
+      Vec3f rgb = rgb_from_hsv(Vec3f(hue, 1.0, 1.0));
+      for (int led = 0; led < NUM_LEDS; led++) {
+        set_color(ptr, rgb);
+      }
 
-            uint8_t* pixels = buffer + (strip * NUM_LEDS * 3);
-            for (int led = 0; led < strip; led++) {
-                pixels[3 * led + 0] = uint8_t(50);
-                pixels[3 * led + 1] = uint8_t(0);
-                pixels[3 * led + 2] = uint8_t(0);
-            }
+      uint8_t *pixels = buffer + (strip * NUM_LEDS * 3);
+      Vec3f red(50 / 255.0f, 0, 0);
+      for (int led = 0; led < strip; led++) {
+        set_color(pixels, red);
+      }
 
         }
->>>>>>> 14ff478 (tweaks to example)
+      }
 
     int sidx = strip_indexes[(freecount / NUM_LEDS) % strip_indexes.size()];
     int pidx = freecount % NUM_LEDS;
