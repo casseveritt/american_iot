abfc5f6f534dd598daae42da77fb78f089ff8ff2
Cass Everitt <cass@xyzw.us>
2025-10-18 09:20:31 -0500

    format

--- xmasv2/hween.cpp
+++ xmasv2/hween.cpp
@@ -55,27 +55,24 @@ void set_color(uint8_t *&ptr, const Vec3f &color) {
   *(ptr++) = (int)(color.z * 255);
 }
 
-int main(int argc, char *argv[])
-{
-    uint8_t buffer[NUM_LEDS * NUM_STRIPS * 3] = {};
-    uint8_t *ptr;
+int main(int argc, char *argv[]) {
+  uint8_t buffer[NUM_LEDS * NUM_STRIPS * 3] = {};
+  uint8_t *ptr;
 
-    std::vector<int> strip_indexes = {0, 1, 2, 3, 4, 5};
+  std::vector<int> strip_indexes = {0, 1, 2, 3, 4, 5};
 
-    // initialize the smi_leds module, starting with a 35% brightness
-    leds_init(NUM_LEDS, 296);
-    printf("compiled for %d strips\n", leds_num_strips());
+  // initialize the smi_leds module, starting with a 35% brightness
+  leds_init(NUM_LEDS, 296);
+  printf("compiled for %d strips\n", leds_num_strips());
 
-    int64_t start = get_time();
-    int count = 0;
-    int64_t freecount = 0;
-    for(float t = 0.0;; t += .0001)
-    {
-        // Manually and slowly build the color buffer. 3 bytes per pixel (RGB)
-        // for NUM_LEDS pixels and NUM_STRIPS strips
-        ptr = buffer;
-        for(int strip = 0; strip < NUM_STRIPS; strip++)
-        {
+  int64_t start = get_time();
+  int count = 0;
+  int64_t freecount = 0;
+  for (float t = 0.0;; t += .0001) {
+    // Manually and slowly build the color buffer. 3 bytes per pixel (RGB)
+    // for NUM_LEDS pixels and NUM_STRIPS strips
+    ptr = buffer;
+    for (int strip = 0; strip < NUM_STRIPS; strip++) {
       float hue = fmod(t + (strip * .04), 1.0);
 
       Vec3f rgb = rgb_from_hsv(Vec3f(hue, 1.0, 1.0));
@@ -88,32 +85,30 @@ int main(int argc, char *argv[])
       for (int led = 0; led < strip; led++) {
         set_color(pixels, red);
       }
-
-        }
-      }
-
-    int sidx = strip_indexes[(freecount / NUM_LEDS) % strip_indexes.size()];
-    int pidx = freecount % NUM_LEDS;
-    Vec3f black(0, 0, 0);
-    ptr = buffer + (sidx * NUM_LEDS + pidx) * 3;
-    set_color(ptr, black);
-
-    // Send the buffer to the SMI buffer
-    leds_set(buffer);
-
-    // Actually send them to the LEDs:
-    leds_send();
-
-    // Sleep for a while
-    // usleep(50000);
-    count++;
-    freecount++;
-    if (count > 1000) {
-      int64_t end = get_time();
-      double delta = (end - start) * 1e-9;
-      printf("fps = %d\n", int(1000 / delta));
-      start = end;
-      count = 0;
     }
+  }
+
+  int sidx = strip_indexes[(freecount / NUM_LEDS) % strip_indexes.size()];
+  int pidx = freecount % NUM_LEDS;
+  Vec3f black(0, 0, 0);
+  ptr = buffer + (sidx * NUM_LEDS + pidx) * 3;
+  set_color(ptr, black);
+
+  // Send the buffer to the SMI buffer
+  leds_set(buffer);
+
+  // Actually send them to the LEDs:
+  leds_send();
+
+  // Sleep for a while
+  // usleep(50000);
+  count++;
+  freecount++;
+  if (count > 1000) {
+    int64_t end = get_time();
+    double delta = (end - start) * 1e-9;
+    printf("fps = %d\n", int(1000 / delta));
+    start = end;
+    count = 0;
   }
 }
