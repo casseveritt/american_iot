1385a0a5c640b1c3a58dc1ff3a933e5301131d5a
Cass Everitt <cass@xyzw.us>
2025-10-23 10:33:29 -0500

    multibuffer pixels

--- xmasv2/hween.cpp
+++ xmasv2/hween.cpp
@@ -17,11 +17,35 @@
 
 using namespace r3;
 
+void color_pixels(std::span<PixInfo> pixInfo, uint8_t *buffer, float t)
+{
+    for (auto p : pixInfo)
+    {
+        float theta = atan2(p.position.z, p.position.x) + M_PI;
+        Vec3f rgb = rgb_from_hsv(Vec3f(fmod(t + theta / (M_PI * 2.0f), 1.0), 1.0, 1.0));
+        float sc = pow(fabs(fmod(p.position.y + 0.3 * t, 0.2f) - 0.1) * 10.0f, 3.0);
+        rgb *= sc;
+        set_color(buffer, p.index, rgb);
+    }
+}
+
+void show_strip_index(uint8_t *buffer)
+{
+    for (int strip = 0; strip < STRIPS; strip++)
+    {
+        uint8_t *pixels = buffer + (strip * PIXELS_PER_STRIP * 3);
+        Vec3f color = rgb_from_hsv(Vec3f(float(strip) / STRIPS, 1.0f, 1.0f));
+        for (int led = 0; led < PIXELS_PER_STRIP; led++)
+        {
+            set_color(buffer, strip * PIXELS_PER_STRIP + led, color);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     constexpr size_t sz = PIXELS_PER_STRIP * STRIPS * 3;
-    uint8_t buffer[sz] = {};
-    uint8_t *ptr;
+    uint8_t buffers[3][sz] = {};
 
     auto pixInfo = get_pix_info();
 
@@ -34,32 +58,16 @@ int main(int argc, char *argv[])
     int64_t start = get_time_nsec();
     int count = 0;
     int64_t freecount = 0;
-    for (float t = 0.0;; t += .004)
+    while (true)
     {
         uint64_t t_ns = get_time_nsec();
         float t_s = t_ns * 1e-9;
 
-        for (auto p : pixInfo)
-        {
-            float theta = atan2(p.position.z, p.position.x) + M_PI;
-            Vec3f rgb = rgb_from_hsv(Vec3f(fmod(t + theta / (M_PI * 2.0f), 1.0), 1.0, 1.0));
-            float sc = pow(fabs(fmod(p.position.y + 0.3 * t_s, 0.2f) - 0.1) * 10.0f, 3.0);
-            rgb *= sc;
-            set_color(buffer, p.index, rgb);
-        }
+        uint8_t *buffer = buffers[freecount % 3];
 
-#define SHOW_STRIP_INDEX 0
-#if SHOW_STRIP_INDEX
-        Vec3f red(0.2f, 0, 0);
-        for (int strip = 0; strip < STRIPS; strip++)
-        {
-            uint8_t *pixels = buffer + (strip * PIXELS_PER_STRIP * 3);
-            for (int led = 0; led < strip; led++)
-            {
-                set_color(buffer, strip * PIXELS_PER_STRIP + led, red);
-            }
-        }
-#endif
+        color_pixels(pixInfo, buffer, t_s);
+
+        // show_strip_index(buffer);
 
         // Send the buffer to the SMI buffer
         leds_set(buffer);
@@ -77,6 +85,6 @@ int main(int argc, char *argv[])
             start = end;
             count = 0;
         }
-        usleep(5000);
+        usleep(800);
     }
 }
