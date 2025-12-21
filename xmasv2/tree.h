88a74ba1a24181afd7d07300c57a5bb0969da0f2
Cass Everitt <cass@xyzw.us>
2025-12-20 23:33:15 -0700

    More tree-specific definitions.

--- xmasv2/tree.h
+++ xmasv2/tree.h
@@ -5,8 +5,13 @@
 
 #include "linear.h"
 
+#if defined(TREE_V3)
+#define STRIPS 4
+#define PIXELS_PER_STRIP 300
+#else
 #define STRIPS 16
 #define PIXELS_PER_STRIP 320
+#endif
 
 struct PixInfo {
   uint32_t index;
